cf2389a9ddca40796561ecae035deaf9c4974658
Felipe Borba <felipe.borba0109@gmail.com>
2024-09-19 21:26:24 -0300

    fazendo requisições

--- lib/services/get_users.dart
+++ lib/services/get_users.dart
@@ -7,6 +7,7 @@ getUsers() async {
     var url = Uri.parse("http://dummyjson.com/users");
     var response = await http.get(url);
     var data = jsonDecode(response.body);
+    await Future.delayed(const Duration(seconds: 1)); //TODO debug spinner
     return data['users'];
   } catch (e) {
     // ignore: use_rethrow_when_possible
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:chat_flutter/data/users_data.dart';
+// import 'package:chat_flutter/data/users_data.dart';
 import 'package:chat_flutter/services/get_users.dart';
 import 'package:chat_flutter/views/detail_page.dart';
 import 'package:flutter/material.dart';
@@ -29,49 +29,62 @@ class HomePage extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: GridView.builder(
-                itemCount: users.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.85,
-                ),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      // getUsers();
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(builder: (context) => DetailPage(
-                      //     user: users[index]
-                      //   )),
-                      // );
-                    },
-                    child: Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CircleAvatar(
-                            radius: 70,
-                            backgroundImage:
-                                NetworkImage(users[index]['image']),
+              child: FutureBuilder(
+            future: getUsers(),
+            builder: (context, AsyncSnapshot snapshot) {
+              var users = snapshot.data;
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              }
+              return GridView.builder(
+                  itemCount: users?.length ?? 0,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.85,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                DetailPage(user: users[index]),
                           ),
-                          Text(
-                            users[index]["firstName"],
-                            style: const TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
+                        );
+                      },
+                      child: Container(
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            CircleAvatar(
+                              radius: 70,
+                              backgroundImage:
+                                  NetworkImage(users[index]['image']),
                             ),
-                          ),
-                          Text(users[index]['company']['title']),
-                        ],
+                            Text(
+                              users[index]["firstName"],
+                              style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(users[index]['company']['title']),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                }),
-          )
+                    );
+                  });
+            },
+          ))
         ],
       ),
     );
